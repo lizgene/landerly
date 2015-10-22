6fadfc48222cb06fca855c3bd9dee2ec18f25562
Liz Hubertz <liz.hubertz@gmail.com>
2015-10-22 17:26:35 +0200

    checkboxes that save price correctly in the price array nested within the preferences json object

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,4 +5,18 @@ class Member < ActiveRecord::Base
     "#{first_name} #{last_name}"
   end
 
+
+  #metaprogramming madness!!
+  #define methods to easily check to see if a user has a given preference in their json object
+  ["subjects", "region", "start_date", "price"].each do |preference|
+    define_method("prefers_#{preference}?") do |preference_type|
+
+      if preferences["#{preference}"] && preferences["#{preference}"].include?("#{preference_type}")
+        return true
+      else
+        return false
+      end
+    end
+  end
+
 end
