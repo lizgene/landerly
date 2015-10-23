df85e6d5f27169c687c5d6074318d14b3726e641
Liz Hubertz <liz.hubertz@gmail.com>
2015-10-23 12:33:24 +0200

    add relationship of trainings_subjects to training and subject models so the relationship now is in working order for new objects

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,3 @@
 class Subject < ActiveRecord::Base
-  has_and_belongs_to_many :trainings
+  has_and_belongs_to_many :trainings,  :join_table => :trainings_subjects
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,3 @@
 class Training < ActiveRecord::Base
-  has_and_belongs_to_many :subjects
+  has_and_belongs_to_many :subjects,  :join_table => :trainings_subjects
 end
